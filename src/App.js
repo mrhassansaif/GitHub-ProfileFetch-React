9b529bbeb81659e5b1fd32017a2dd306f82ca92b
Muhammad Hassan Bin Saif <hassansaif0ki@gmail.com>
2021-09-20 22:57:53 +0500

    Install "Axios" before running build

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import "./App.css";
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+
+
+function App() {
+    const [userN, setuserN] = useState([])
+    const [userData, setuserData] = useState([])
+
+    const userEvent = (event) => {
+        setuserN(event.target.value)
+        console.log(userN)
+    }
+
+    useEffect(() => {
+        axios.get("https://api.github.com/users/" + userN).then((res) => {
+            console.log(res.data);
+            setuserData(res.data)
+            console.log(userData)
+        });
+    }, [userN]);
+
+    return (
+        <>
+
+            <div className="wrapper" >
+                <div class="search" id="search">
+                    <input type="text" name="" id="user" onChange={userEvent}
+                    />
+                </div>
+                <div className="profile-card js-profile-card">
+                    <div className="profile-card__img">
+                        <img src={userData.avatar_url} alt="profile card" />
+                    </div>
+                    <div className="profile-card__cnt js-profile-cnt">
+                        <div className="profile-card__name">{userData.name}</div>
+                        <div className="profile-card__txt">Company <strong>{userData.company}</strong></div>
+                        <div className="profile-card__txt">Bio <strong>{userData.bio}</strong></div>
+                        <div className="profile-card-inf">
+                            <div className="profile-card-inf__item">
+                                <div className="profile-card-inf__title">{userData.followers}</div>
+                                <div className="profile-card-inf__txt">Followers</div>
+                            </div>
+                            <div className="profile-card-inf__item">
+                                <div className="profile-card-inf__title">{userData.following}</div>
+                                <div className="profile-card-inf__txt">Following</div>
+                            </div>
+                            <div className="profile-card-inf__item">
+                                <div className="profile-card-inf__title">{userData.public_repos}</div>
+                                <div className="profile-card-inf__txt">Public
+                                    Repositories</div>
+                            </div>
+                        </div>
+                        <div className="profile-card-ctr">
+                            <button className="profile-card__button button--blue js-message-btn">Contact Me</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
